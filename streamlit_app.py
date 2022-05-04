8ad449901275b0e7b8bbeb4893be6fcb1690441e
catja7 <104902899+catja7@users.noreply.github.com>
2022-05-04 14:36:43 +0200

    Update streamlit_app.py
    Korrektur: set_index

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text('\N{bottle with popping cork}')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #Auswahlliste
 streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
