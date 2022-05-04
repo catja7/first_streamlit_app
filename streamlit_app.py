23d5b358134a22c9fc8568a1fcfa230e7db2b468
catja7 <104902899+catja7@users.noreply.github.com>
2022-05-04 14:33:53 +0200

    Update streamlit_app.py
    Auswahlliste

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,6 @@ streamlit.text('\N{bottle with popping cork}')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Auswahlliste
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
