6fdbb4b93da50c5425a7e5c75adbf4d6d7a4a598
catja7 <104902899+catja7@users.noreply.github.com>
2022-05-04 14:16:55 +0200

    Update streamlit_app.py
    weitere zeilen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Essen01')
+streamlit.text('Essen02')
+streamlit.text('Essen03')
